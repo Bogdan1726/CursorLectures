b6fed344c1ee5caff03ec2a1e10ed82537c0303b
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-04-02 23:55:56 +0300

    added files of the lectures 3-9

---
+++ context_managers/ex3.py
@@ -0,0 +1,13 @@
+# file = open("test.txt")
+# file.close()
+
+from ex4 import Open2
+#
+# with open("test.txt", "w") as file:
+#     file.write("Hello")
+#
+# print("end")
+
+with Open2("test.txt", "w") as file:
+    file.write("Hello123111")
+    raise NameError
