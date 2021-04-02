8ed71e8ee6c9a40682c0b58231d5c7c7ef3c59d8
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-04-03 00:14:17 +0300

    added files of the except_logging

---
+++ exceptions_logging/ex2.py
@@ -0,0 +1,10 @@
+
+def func():
+    print("asdasd")
+    print("asdasd")
+    func()
+    print("asdasd")
+
+func()
+
+raise StopIteration
---
+++ exceptions_logging/ex5.py
@@ -0,0 +1,14 @@
+
+try:
+    print("try")
+    # raise Exception
+    print("Exception12")
+
+    pass
+except Exception:
+    print("Exception")
+    pass
+else:
+    print("DONE")
+finally:
+    print("end")
