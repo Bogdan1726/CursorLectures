5a0181ac02f59f14a3b41f38e0c887bed9e10baa
Bohdan Rozhniatovskiy <bogdan24ro@gmail.com>
2021-04-03 11:13:49 +0300

    added files of the testing

---
+++ practice_testing/exceptions.py
@@ -0,0 +1,19 @@
+
+class BaseException(Exception):
+    pass
+
+
+class PswLenError(BaseException):
+    pass
+
+
+class PswWrongSymbolsError(BaseException):
+    pass
+
+
+class NameWrongSymbolsError(BaseException):
+    pass
+
+
+class NameLenError(BaseException):
+    pass
